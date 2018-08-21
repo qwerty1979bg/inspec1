17c9b10e11a86e2a8b9a6134f5fc3e43ae4c310c
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2018-08-21 17:24:40 +0300

    adding description to the passwd control file

--- russians/controls/passwd.rb
+++ russians/controls/passwd.rb
@@ -3,6 +3,7 @@ title 'passwd backdoor'
 # you add controls here
 control "passwd-1.0" do
   title "Check for a backdoor in /etc/passwd"
+  desc "Do some basic checks on /etc/passwd"
   describe file('/etc/passwd') do
     it { should be_owned_by 'root' }
   end
